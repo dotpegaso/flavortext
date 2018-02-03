84c7915bc123f7f816601fcb2e59285f5ec53b63
André L Benatti <dotpegaso@gmail.com>
2018-02-03 21:42:56 +0000

    added artist and set, change some animations, better card fetch from API

--- modules/home/js/homeController.js
+++ modules/home/js/homeController.js
@@ -2,12 +2,17 @@ angular.module('home', [])
 
 .controller('homeController', function($scope, $http){
     
-    $scope.i = Math.floor(Math.random() * 100) + 0;
-    
-    $http.get('https://api.magicthegathering.io/v1/cards?flavor=true&color=red').then(function(res){
+    $http.get('https://api.magicthegathering.io/v1/cards?flavor=a|e|i|o|u&random=true').then(function(res){
+        
+        $scope.i = Math.floor(Math.random() * res.data.cards.length) + 0;
         $scope.flavor = res.data.cards[$scope.i].flavor;
         $scope.name = res.data.cards[$scope.i].name;
         $scope.color = res.data.cards[$scope.i].colors;
         $scope.img = res.data.cards[$scope.i].imageUrl;
+        $scope.setName = res.data.cards[$scope.i].setName;
+        $scope.artist = res.data.cards[$scope.i].artist;
+        $scope.legalities = res.data.cards[$scope.i].legalities;
+        
+        console.log(res.data.cards);
     })
 })
