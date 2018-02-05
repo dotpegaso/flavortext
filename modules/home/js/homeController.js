dab9df0189600d70a35686047d8ddb4494c2b755
André L Benatti <dotpegaso@gmail.com>
2018-02-05 16:21:24 +0000

    fixed some mobile css and stuff

--- modules/home/js/homeController.js
+++ modules/home/js/homeController.js
@@ -17,19 +17,16 @@ angular.module('home', [])
     
     $scope.quote = $scope.quoteList[Math.floor(Math.random() * $scope.quoteList.length) + 0];
     
-    $scope.getFlavor = function(){
-        $http.get('https://api.magicthegathering.io/v1/cards?flavor=a|e|i|o|u&random=true').then(function(res){
+    $http.get('https://api.magicthegathering.io/v1/cards?flavor=a|e|i|o|u&random=true').then(function(res){
             
-            $scope.i = Math.floor(Math.random() * res.data.cards.length) + 0;
-            $scope.flavor = res.data.cards[$scope.i].flavor;
-            $scope.name = res.data.cards[$scope.i].name;
-            $scope.color = res.data.cards[$scope.i].colors;
-            $scope.img = res.data.cards[$scope.i].imageUrl;
-            $scope.setName = res.data.cards[$scope.i].setName;
-            $scope.artist = res.data.cards[$scope.i].artist;
-            
-        });
-    }
+        $scope.i = Math.floor(Math.random() * res.data.cards.length) + 0;
+        $scope.flavor = res.data.cards[$scope.i].flavor;
+        $scope.name = res.data.cards[$scope.i].name;
+        $scope.color = res.data.cards[$scope.i].colors;
+        $scope.img = res.data.cards[$scope.i].imageUrl;
+        $scope.setName = res.data.cards[$scope.i].setName;
+        $scope.artist = res.data.cards[$scope.i].artist;
+        
+    });
     
-    $scope.getFlavor();
 })
