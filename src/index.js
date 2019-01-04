81bbf5b63aaba84f9b235e41e5e150914d8775c3
dotpegaso <dotpegaso@gmail.com>
2019-01-04 14:27:44 -0200

    correções de font, ativação de serviceworker e otimização de imagens de fundo

--- src/index.js
+++ src/index.js
@@ -56,8 +56,4 @@ class Mtg extends Component{
 }
 
 ReactDOM.render(<Mtg />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
